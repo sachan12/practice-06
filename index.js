ed20f0717051f31a5547e7da0e69a206575a20fc
sachan12 <sachinnanda427@gmail.com>
2024-03-11 12:30:00 -0700

    add files

---
+++ index.js
@@ -0,0 +1,43 @@
+ let hatPrice = 100;
+
+ console.log( `Hat price : ${hatPrice}`);
+
+let bootPrice = '100';
+ console.log( `Boot Price : ${bootPrice}`);
+
+ console.log(hatPrice + Number(bootPrice));
+
+//  console.log(100);
+
+//conditional statements (if , elseif , else) ; (relational Operators ==, ===,<,>)
+
+// let chapalPrice = 200
+// if (chapalPrice ==100){
+//     console.log("you are right");
+// } else if (chapalPrice == 200){
+//     console.log('price is 100');
+// }
+// else{
+//     console.log('you are wrong');
+// }
+
+// let airbudPrice = 3000
+// if (airbudPrice == 3000){
+//     console.log("You are wrong");
+// } else if (airbudPrice == 2000){
+//     console.log("you are right");
+// } 
+
+// Symbol Data types ;
+// let greeting1 = Symbol('Hello');
+// let greeting2 = Symbol('Hello');
+// console.log(greeting1 == greeting2);
+
+
+
+
+
+
+
+
+
